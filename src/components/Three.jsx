e2165dbd130c27de68bb5a1b59ef60c2934f2f40
davidingles <davidingleslopez@gmail.com>
2025-03-04 16:52:30 +0100

    FOTOCARD333

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -5,7 +5,7 @@ import { Stats, OrbitControls, Environment, useGLTF, Clone, Html, ContactShadows
 import { useControls } from 'leva';
 
 const Models = [
-  { title: 'FOTOCARD', url: './FOTOCARD.glb', miEscala: .71, miPosicion: 0 },
+  { title: 'FOTOCARD1', url: './FOTOCARD1.glb', miEscala: .71, miPosicion: 0 },
   // { title: 'CANARA2', url: './CANARA2.glb', miEscala: 0.5, miPosicion: 0 },
   // { title: 'CANARA3', url: './CANARA3.glb', miEscala: 0.5, miPosicion: 0 },
   // { title: 'CANARA4', url: './CANARA4.glb', miEscala: 0.5, miPosicion: 0 },
